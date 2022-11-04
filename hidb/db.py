81db40d88f682509e3672609f6e1aa77e6f69b58
Donald Burr <dburr@borg-cube.com>
2022-11-04 14:27:18 -0700

    Finish basic search implementation
    Probably could do with some error checking/sanitation/etc. but it more or less works now

--- hidb/db.py
+++ hidb/db.py
@@ -3,7 +3,6 @@ import sqlite3
 import click
 from flask import current_app, g
 
-
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
@@ -13,7 +12,6 @@ def get_db():
         g.db.row_factory = sqlite3.Row
 
     return g.db
-
 
 def close_db(e=None):
     db = g.pop('db', None)
--- hidb/search.py
+++ hidb/search.py
@@ -17,34 +17,40 @@ def index():
 @bp.route('/search/run_search', methods=('POST',))
 def run_search():
     if request.method == 'POST':
-
-      print(request.form)
-
       do_search_model_no = request.form.get('search_model_no')
       do_search_description = request.form.get('search_description')
       do_search_locations = request.form.get('search_locations')
       do_search_sublocation = request.form.get('search_sublocations')
 
-      what = ""
-      if do_search_model_no == "search_model_no":
-        what += "model_no: " + request.form['model_no'] + "<br />"
-      if do_search_description == "search_description":
-        what += "description: " + request.form['description'] + "<br />"
-      if do_search_locations == "search_locations":
-        what += "locations: "
-        locs = request.form.getlist('locations')
-        for l in locs:
-          what += l + " "
-        what += "<br />"
-      if do_search_sublocation == "search_sublocation":
-        what += "sublocation: " + request.form['sublocation'] + "<br />"
+      query = 'SELECT i.id, model_no, description, qty, cost, date_added, location, sublocation FROM items i WHERE '
 
-      if len(what) == 0:
-        error = "Boogers"
+      if do_search_model_no == "search_model_no":
+        query += "model_no LIKE '%%%s%%' AND " % request.form['model_no']
+      if do_search_description == "search_description":
+        query += "description LIKE '%%%s%%' AND " % request.form['description']
+      if do_search_locations == "search_locations":
+        query += "location IN (" + ",".join(request.form.getlist('locations')) + ") AND "
+      if do_search_sublocation == "search_sublocation":
+        query += "sublocation LIKE '%%%s%%'" + request.form['sublocation']
+
+      if query.endswith('AND '):
+        query = query[:-4]
+
+      db = get_db()
+      cursor = db.cursor()
+      cursor.execute(query)
+      db.commit()
+      results = cursor.fetchall()
+
+      # print(len(results))
+      # print(results)
+
+      if len(results) == 0:
+        error = "No matching items were found."
       else:
         error = None
 
       if error is not None:
-        return render_template('search/error.html', error=error)
+        return render_template('search/error.html', error=error, query=query)
       else:
-        return render_template('search/results.html', results=what)
+        return render_template('search/results.html', query=query, results=results)
