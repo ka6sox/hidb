9e9f3fa2da13e5acda0f015bd52a84b2319e82f2
Donald Burr <dburr@borg-cube.com>
2022-11-04 12:40:40 -0700

    Add more to the search stub
    * actual search form
    * error and results pages

--- hidb/search.py
+++ hidb/search.py
@@ -5,13 +5,21 @@ from werkzeug.exceptions import abort
 
 from hidb.auth import login_required
 from hidb.db import get_db
+from .locations import get_locations
 
 bp = Blueprint('search', __name__)
 
 @bp.route('/search')
 def index():
-    return render_template('search/index.html')
+  locations = get_locations()
+  return render_template('search/index.html', locations=locations)
 
 @bp.route('/search/run_search', methods=('GET', 'POST'))
 def run_search():
-    return render_template('search/results.html')
+    if request.method == 'POST':
+      error = "Boogers"
+
+      if error is not None:
+        return render_template('search/error.html', error=error)
+      else:
+        return render_template('search/results.html', results="boogers")
