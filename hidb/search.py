d3272f25cc82624243f447e10c4e4099272660c2
Donald Burr <dburr@borg-cube.com>
2022-11-04 13:01:02 -0700

    More work on the search stub
    * add a results page
    * grab and process values of search form fields

--- hidb/search.py
+++ hidb/search.py
@@ -14,12 +14,37 @@ def index():
   locations = get_locations()
   return render_template('search/index.html', locations=locations)
 
-@bp.route('/search/run_search', methods=('GET', 'POST'))
+@bp.route('/search/run_search', methods=('POST',))
 def run_search():
     if request.method == 'POST':
-      error = "Boogers"
+
+      print(request.form)
+
+      do_search_model_no = request.form.get('search_model_no')
+      do_search_description = request.form.get('search_description')
+      do_search_locations = request.form.get('search_locations')
+      do_search_sublocation = request.form.get('search_sublocations')
+
+      what = ""
+      if do_search_model_no == "search_model_no":
+        what += "model_no: " + request.form['model_no'] + "<br />"
+      if do_search_description == "search_description":
+        what += "description: " + request.form['description'] + "<br />"
+      if do_search_locations == "search_locations":
+        what += "locations: "
+        locs = request.form.getlist('locations')
+        for l in locs:
+          what += l + " "
+        what += "<br />"
+      if do_search_sublocation == "search_sublocation":
+        what += "sublocation: " + request.form['sublocation'] + "<br />"
+
+      if len(what) == 0:
+        error = "Boogers"
+      else:
+        error = None
 
       if error is not None:
         return render_template('search/error.html', error=error)
       else:
-        return render_template('search/results.html', results="boogers")
+        return render_template('search/results.html', results=what)
