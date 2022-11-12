8149bfb7364d6bfce8c82c486fe00de0bf2ab6dd
Donald Burr <dburr@borg-cube.com>
2022-11-11 22:00:24 -0800

    Add some sql to create sample data (currently commented out)

--- hidb/schema.sql
+++ hidb/schema.sql
@@ -42,12 +42,12 @@ CREATE TABLE items (
   FOREIGN KEY (location) REFERENCES locations (id)
 );
 
-INSERT INTO users (username, password) VALUES ('admin', 'pbkdf2:sha256:260000$Jm6usB1WoMeeuz5i$ce04b52727d032235104b2c7dd932ac6f5f8c7bbe7076d65eefb0a8fbe807c94');
+-- INSERT INTO users (username, password) VALUES ('admin', 'pbkdf2:sha256:260000$Jm6usB1WoMeeuz5i$ce04b52727d032235104b2c7dd932ac6f5f8c7bbe7076d65eefb0a8fbe807c94');
 
-INSERT INTO rooms (creator_id, description) VALUES (1, 'Bedroom');
-INSERT INTO rooms (creator_id, description) VALUES (1, 'Office');
-INSERT INTO rooms (creator_id, description) VALUES (1, 'Living Room');
+-- INSERT INTO rooms (creator_id, description) VALUES (1, 'Bedroom');
+-- INSERT INTO rooms (creator_id, description) VALUES (1, 'Office');
+-- INSERT INTO rooms (creator_id, description) VALUES (1, 'Living Room');
 
-INSERT INTO locations (creator_id, description) VALUES (1, 'Shelf');
-INSERT INTO locations (creator_id, description) VALUES (1, 'Drawer');
-INSERT INTO locations (creator_id, description) VALUES (1, 'Cabinet');
+-- INSERT INTO locations (creator_id, description) VALUES (1, 'Shelf');
+-- INSERT INTO locations (creator_id, description) VALUES (1, 'Drawer');
+-- INSERT INTO locations (creator_id, description) VALUES (1, 'Cabinet');
