eff342e34dc08641cce1c9067d53be1eb796269c
Donald Burr <dburr@borg-cube.com>
2026-01-29 18:27:52 -0800

    Init database if it doesn't exist

--- hidb/run.py
+++ hidb/run.py
@@ -1,7 +1,12 @@
 from . import create_app
 from .settings import config
+from .models import db
 
 application = create_app()
+
+# Initialize database tables if they don't exist
+with application.app_context():
+    db.create_all()
 
 if config.ENVIRONMENT == "development":
     application.run(host=config.HOST, port=config.PORT, debug=True)
