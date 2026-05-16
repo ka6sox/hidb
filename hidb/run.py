26fa7de46a6feb329c4a4ce63f67ae5c9cbfb99f
Derek Young <derek,young@gmail.com>
2026-05-15 23:40:57 -0700

    only expose WSGI endpoint, update db separately

--- hidb/run.py
+++ hidb/run.py
@@ -1,12 +1,3 @@
 from . import create_app
-from .settings import config
-from .models import db
 
 application = create_app()
-
-# Initialize database tables if they don't exist
-with application.app_context():
-    db.create_all()
-
-if config.ENVIRONMENT == "development":
-    application.run(host=config.HOST, port=config.PORT, debug=True)
